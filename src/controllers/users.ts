faa43276e1938785c6598d47692703b0c1096fb2
Brody <49502879+meloknight@users.noreply.github.com>
2024-01-16 09:14:35 -0800

    Added update functionality.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { deleteUserById, getUsers } from '../db/users'
+import { deleteUserById, getUsers, getUserById } from '../db/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -32,3 +32,30 @@ export const deleteUser = async (req: express.Request,  res: express.Response) =
         
     }
 }
+
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params
+        const { username } = req.body
+
+        if(!username) {
+            return res.sendStatus(400)
+        }
+
+        const user = await getUserById(id)
+
+        user.username = username
+        await user.save()
+
+        return res.sendStatus(200).json(user).end()
+
+    } catch(error) {
+        console.log(error);
+        return res.sendStatus(400)
+        
+    }
+}
+
+
+
